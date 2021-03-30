874af9e3cf6f6f1668b1ac973a92d241a6023e57
Alex Alikhani <alikhanialex@gmail.com>
2021-03-30 20:20:39 +0100

    Setting up basic page for Quiz, and API for quiz

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const QuestionCard = () =>  <div>Question Card</div>
+// Declaring types of props
+type Props = {
+  question: string;
+  answers: [];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+//React.FC signals that this type will be a functional component
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }) => (
+  <div>
+    <p className="number">Question: {questionNr} / {totalQuestions}</p>
+    <p dangerouslySetInnerHTML={{__html: question }} />
+    <div>
+  {answers.map(answer => (
+    <div>
+      <button disabled={userAnswer} onClick={callback}/>
+      <span dangerouslySetInnerHTML={{ __html: answer }} />
+      </div>
+  ))}
+    </div>
+  </div>
+);
 
 export default QuestionCard;
